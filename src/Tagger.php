6c06e4262d32fe1a270b24c7cf304de99c1ef3ab
Leonardo Forero <gerencia@micrositios.net>
2022-12-16 20:02:28 -0500

    fix(Composer): Fix package name

--- src/Tagger.php
+++ src/Tagger.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace Micrositios\Names\App;
 
+use voku\helper\ASCII;
+
 final class Tagger
 {
     public function tag(array $name): array
     {
         $types = ['N', 'L', 'C'];
         foreach ($name as $part) {
-            $parts[] = [$part, $types[rand(0, count($types) - 1)]];
+            $type = $types[rand(0, count($types) - 1)];
+            $parts[] = [$part, ASCII::to_transliterate(mb_strtolower($part, 'UTF-8')), $type];
         }
         return $parts;
     }
