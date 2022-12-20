8dcd21c3ee87e821497bd173d330f3a4cf854d28
Leonardo Forero <gerencia@micrositios.net>
2022-12-20 11:01:42 -0500

    feat(Compacter): Improve compact rules

--- src/Compacter.php
+++ src/Compacter.php
@@ -10,15 +10,14 @@ final class Compacter
     {
         $compactables = ['C'];
 
-        if (!array_intersect($compactables, array_column($values, 'type'))) {
-            return $values;
-        }
+        // if (!array_intersect($compactables, array_column($values, 'type'))) {
+        //     return $values;
+        // }
 
-        // foreach ($compactables as $compactable) {
+        // Compactar todos los C en un solo C
         $prev = null;
         $tmpValues = [];
         foreach ($values as $value) {
-            // if (in_array($value['type'], $compactables) && $value['type'] === $prev) {
             if (in_array($prev, $compactables)) {
                 $index = count($tmpValues) - 1;
                 $original = $tmpValues[$index]['original'] . ' ' . $value['original'];
@@ -30,8 +29,70 @@ final class Compacter
             $prev = $value['type'];
         }
         $values = $tmpValues;
-        // }
 
-        return $values;
+        // Compactar todos los CL como L
+        $prev = null;
+        $tmpValues = [];
+        foreach ($values as $value) {
+            if ($value['type'] === 'L' && $prev === 'C') {
+                $index = count($tmpValues) - 1;
+                $original = $tmpValues[$index]['original'] . ' ' . $value['original'];
+                $modified = $tmpValues[$index]['modified'] . ' ' . $value['modified'];
+                $tmpValues[$index] = ['original' => $original, 'modified' => $modified, 'type' => $value['type']];
+            } else {
+                $tmpValues[] = ['original' => $value['original'], 'modified' => $value['modified'], 'type' => $value['type']];
+            }
+            $prev = $value['type'];
+        }
+        $values = $tmpValues;
+
+        // Reemplazar el primer N => A y el primer L => B
+        $flagA = null;
+        $flagB = null;
+        $tmpValues = [];
+        foreach ($values as $value) {
+            if ($value['type'] === 'N' && !$flagA) {
+                $value['type'] = 'A';
+                $flagA = true;
+            }
+            if ($value['type'] === 'L' && !$flagB) {
+                $value['type'] = 'B';
+                $flagB = true;
+            }
+            $tmpValues[] = ['original' => $value['original'], 'modified' => $value['modified'], 'type' => $value['type']];
+        }
+        $values = $tmpValues;
+
+        // Compactar todos los N y L restantes
+        $prev = null;
+        $tmpValues = [];
+        $compactables = ['N', 'L'];
+        foreach ($values as $value) {
+            if (in_array($prev, $compactables) && $prev === $value['type']) {
+                $index = count($tmpValues) - 1;
+                $original = $tmpValues[$index]['original'] . ' ' . $value['original'];
+                $modified = $tmpValues[$index]['modified'] . ' ' . $value['modified'];
+                $tmpValues[$index] = ['original' => $original, 'modified' => $modified, 'type' => $value['type']];
+            } else {
+                $tmpValues[] = ['original' => $value['original'], 'modified' => $value['modified'], 'type' => $value['type']];
+            }
+            $prev = $value['type'];
+        }
+        $values = $tmpValues;
+
+
+        $tmpValues = [];
+        foreach ($values as $value) {
+            if ($value['type'] === 'A') {
+                $value['type'] = 'N';
+            }
+            if ($value['type'] === 'B') {
+                $value['type'] = 'L';
+            }
+            $tmpValues[] = ['original' => $value['original'], 'modified' => $value['modified'], 'type' => $value['type']];
+        }
+        $values = $tmpValues;
+
+        return $tmpValues;
     }
 }
