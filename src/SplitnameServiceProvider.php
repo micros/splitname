49f8182019407395ec775a726e1e3a5c6ffba574
Leonardo Forero <gerencia@micrositios.net>
2023-03-15 22:09:22 -0500

    fix(ServiceProvider): Delete test route

--- src/SplitnameServiceProvider.php
+++ src/SplitnameServiceProvider.php
@@ -8,7 +8,6 @@ class SplitnameServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');
     }
     public function register()
